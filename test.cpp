a2bb247e608d1004d48ea027c0e8eadbdd291a74
lph <lph995201@163.com>
2022-03-26 13:14:14 -0700

    added cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 26 Mar 2022 01:12:41 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+
+using namespace std;
+int main(){
+    cout << "hello";
+}
